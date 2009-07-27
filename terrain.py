e70e8a08f985e7effb618142b68a459aa03deaa9
Johannes Dewender <jonerian@JonnyJD.net>
2009-07-27 22:17:29 +0000

    Fehlerkontrolle Landschaftsaktualisierung
    git-svn-id: http://svn.kraehen.org/tools/nest/trunk@183 e0e46c49-be69-4f5a-ad62-21024a331aea

--- terrain.py
+++ terrain.py
@@ -9,14 +9,20 @@ import util
 
 print "Content-type: text/plain\n"
 
-def add_to_query(sql, fields):
+def add_to_query(fields):
     if len(fields) >= 4:
         level = fields[0]
         x = fields[1]
         y = fields[2]
         terrain = fields[3]
-        sql += "(" + x + "," + y + ",'" + level + "'," + terrain + "),"
-    return sql
+        if not x.isdigit() or not y.isdigit() or not terrain.isdigit():
+            return None
+        elif not level.isalnum() or len(level) >= 2:
+            return None
+        else:
+            return "(" + x + "," + y + ",'" + level + "'," + terrain + "),"
+    else:
+        return None
 
 form = cgi.FieldStorage()
 if form.has_key("data"):
@@ -25,13 +31,19 @@ if form.has_key("data"):
     sql = "REPLACE INTO felder (x, y, level, terrain) VALUES "
 
     lines = form["data"].value.splitlines()
+    correct = 0
     for line in lines:
         fields = line.split()
-        sql = add_to_query(sql, fields)
+        current = add_to_query(fields)
+        if current != None:
+            sql += current
+            correct += 1
+        else:
+            print '"', line, '" enthielt Fehler <br />'
     sql = sql.rstrip(',')
     try:
         cursor.execute(sql)
-        print len(lines), 'Felder wurden eingetragen'
+        print correct, 'Felder wurden eingetragen'
     except rbdb.Error, e:
         util.print_error(e)
 

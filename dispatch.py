09df8694352bd4450b1c7c2408c5138803d884c3
Johannes Dewender <jonerian@JonnyJD.net>
2009-09-10 17:03:24 +0000

    nest: Sektionen werden jetzt aus dem gesendeten extrahiert
    git-svn-id: http://svn.kraehen.org/tools/nest/trunk@309 e0e46c49-be69-4f5a-ad62-21024a331aea

--- dispatch.py
+++ dispatch.py
@@ -4,19 +4,41 @@
 #cgitb.enable()
 
 import cgi
+import re
 from terrain import Terrain
 
+def get_sections(data):
+    lines = data.splitlines();
+    sections = dict()
+    pos = 0;
+    while pos < len(lines):
+        result = re.match("^<([a-z]*)>", lines[pos])
+        if result:
+            section = result.group(1)
+            start = pos
+            while pos < len(lines) and not lines[pos] == "</"+section+">":
+                pos += 1
+            sections[section] = "\n".join(lines[start+1:pos])
+        pos += 1
+    if len(sections) == 0:
+        sections["felder"] = "\n".join(lines)
+    return sections
 
 if __name__ == '__main__':
     form = cgi.FieldStorage()
-    print "Content-type: text/plain\n"
+    print "Content-type: text/html\n"
 
     if form.has_key("data"):
-        terrain = Terrain()
-        terrain.process(form["data"].value)
+        data = form["data"].value
+        sections = get_sections(data)
+
+        if "felder" in sections:
+            terrain = Terrain()
+            terrain.process(sections["felder"])
+        if "armeen" in sections:
+            print "Armeen werden noch nicht gespeichert", "<br />"
     else:
-        terrain = Terrain()
-        terrain.process("")
+        print "Es wurden keine Daten gesendet."
 
 
 # vim:set shiftwidth=4 expandtab smarttab:

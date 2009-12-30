e0014fd31a057bbcabe3b8db6ed88c2d1aa06292
Johannes Dewender <jonerian@JonnyJD.net>
2009-12-30 02:07:46 +0000

    nest: + ausgabemodul
    git-svn-id: http://svn.kraehen.org/tools/nest/trunk@419 e0e46c49-be69-4f5a-ad62-21024a331aea

---
+++ ausgabe.py
@@ -0,0 +1,37 @@
+"""Einige Klassen und Funktionen zur HTML-Ausgabe"""
+
+from types import *
+
+class Tabelle():
+    """Eine HTML-Tabelle"""
+
+    def __init__(self):
+        self.__columns = []
+        self.__lines = []
+
+    def addColumn(self, col):
+        self.__columns.append(col)
+
+    def addLine(self, cols):
+        self.__lines.append(cols)
+
+    def length(self):
+        return len(self.__lines)
+
+    def show(self):
+        print '<table>'
+        print '<tr>'
+        for col in self.__columns:
+            print '<th>' + col + '</th>'
+        print '</tr>'
+        for line in self.__lines:
+            print '<tr>'
+            for col in line:
+                if type(col) is IntType:
+                    print '<td align="right">' + str(col) + '</td>'
+                else:
+                    print '<td>' + str(col) + '</td>'
+            print '</tr>'
+        print '</table>'
+
+# vim:set shiftwidth=4 expandtab smarttab:

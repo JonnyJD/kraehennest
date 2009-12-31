0d56c3402d493063c64302af802163157b30efad
Johannes Dewender <jonerian@JonnyJD.net>
2009-12-31 00:10:56 +0000

    nest: einfachere example config
    git-svn-id: http://svn.kraehen.org/tools/nest/trunk@433 e0e46c49-be69-4f5a-ad62-21024a331aea

--- config-example.py
+++ config-example.py
@@ -10,12 +10,14 @@ db = ""
 kraehen = []
 tw = []
 
+choose_kraehen = True
+
 def is_kraehe(username):
     #return username in kraehen
-    return True
+    return choose_kraehen
 
 def is_tw(username):
     #return username in tw
-    return False
+    return not choose_kraehen
 
 # vim:set shiftwidth=4 expandtab smarttab:
